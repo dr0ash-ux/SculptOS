043f3563ca6a6381e538f637da2afac8bf5dbc1e
dr0ash-ux <doc.ashwarya@gmail.com>
2026-08-13 17:26:14 +0530

    feat: connect imaging workspace to dashboard navigation

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ImagingBridge from './ImagingBridge.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ImagingBridge />
   </StrictMode>,
 )
